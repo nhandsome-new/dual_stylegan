3f6f672a507070a857be2603137c52b1bf014d4f
nhandsome-new <hanbs044@gmail.com>
2022-04-20 02:24:28 +0000

    -asd

--- export_dual_stylegan_onnx.py
+++ export_dual_stylegan_onnx.py
@@ -1,3 +1,5 @@
+import os
+import wget
 import numpy as np
 import types
 import argparse
@@ -24,13 +26,13 @@ def parse_args(parser):
     # model_dir / style / align_face / 
     parser.add_argument('--model_dir', type=str, required=True,
                         help='full path to the models dir')
-    parser.add_argument('--style', type=str, required=True, default='cartoon'
+    parser.add_argument('--style', type=str, required=True, default='cartoon',
                         help="style name, default is cartoon ")
 
-def run_alignment(image_path):
+def run_alignment(image_path, args):
     import dlib
     from model.encoder.align_all_parallel import align_face
-    modelname = os.path.join(MODEL_DIR, 'shape_predictor_68_face_landmarks.dat')
+    modelname = os.path.join(args.model_dir, 'shape_predictor_68_face_landmarks.dat')
     if not os.path.exists(modelname):
         import wget, bz2
         wget.download('http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2', modelname+'.bz2')
@@ -41,6 +43,26 @@ def run_alignment(image_path):
     aligned_image = align_face(filepath=image_path, predictor=predictor)
     return aligned_image
 
+
+# for test
+class parser() :
+    model_dir = '/DualStyleGAN/checkpoint',
+    style_img = '/DualStyleGAN/data/cartoon/images/train/Cartoons_00167_01.jpg'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu',
+    
 if __name__ == '__main__':
+    
+    args = parser()
+    
+    transform = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(256),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+    ]
+)
+
     test_input_img = '/content/DualStyleGAN/data/content/unsplash-rDEOVtE7vOs.jpg'
-    run_alignment(run_alignment)
+    test_input_img = run_alignment(test_input_img, args)
+    test_input_img = transform(run_alignment(test_input_img)).unsqueeze(dim=0).to(args.device)
